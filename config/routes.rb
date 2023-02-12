2f9af0ca729fba07dfb7f8239464e6a6fb3048f3
Sebastian Rangel <sebastianrangelquintero@gmail.com>
2023-02-12 10:42:29 -0500

    Add category model and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
    get 'about', to: 'pages#about'
    root "pages#home"
+   get 'pages/home', to: 'pages#home', as: 'pages_home'
+   get 'pages/about', to: 'pages#about', as: 'pages_about'
    resources :articles
    get 'signup', to: 'users#new'
    resources :users, except: [:new]
